302f56458a07e73a6b7ed26b3944a10ee2058661
AndrewHanson1 <47340852+AndrewHanson1@users.noreply.github.com>
2019-05-07 19:37:05 -0700

    here

--- hotrest.js
+++ hotrest.js
@@ -13,68 +13,71 @@ app.use(express.json());
 
 //obj
 var reservations = [
-    {
-        reservationName: "first",
-        phoneNumber: "1324234234",
-        email: "werwer@gmail.com",
-        uniqueID: "23"
-    }
+  {
+    reservationName: "first person",
+    phoneNumber: "1324234234",
+    email: "werwer@gmail.com",
+    uniqueID: "important"
+  }
 ];
 
 var waitingList = [
 
-    {},
 
-    {}
-    
 ];
 
 //routing
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/make", function(req, res) {
+app.get("/make", function (req, res) {
   res.sendFile(path.join(__dirname, "make.html"));
 });
 
-app.get("/view", function(req, res) {
+app.get("/view", function (req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
 // Displays all reservations
-app.get("/api/reservations", function(req, res) {
+app.get("/api/reservations", function (req, res) {
   return res.json(reservations);
 });
 
 //api's
 
 // Displays all reservations
-app.get("/api/reservations", function(req, res) {
-    return res.json(reservations);
-  });
-  
-  // Displays all waiting list
-  app.get("/api/waitinglist", function(req, res) {
-      return res.json(waitingList);
-    
-  });
-  
-  // Create New reservation
-app.post("/api/reservations", function(req, res) {
-   
-    var newReservation = req.body;
-  
-    console.log(newReservation);
-  
+app.get("/api/reservations", function (req, res) {
+  return res.json(reservations);
+});
+
+// Displays waiting list
+app.get("/api/waitinglist", function (req, res) {
+  return res.json(waitingList);
+
+});
+
+// Create New reservation or waiting list
+app.post("/api/reservations", function (req, res) {
+
+  var newReservation = req.body;
+
+  console.log(newReservation);
+  if (reservations.length < 5) {
     reservations.push(newReservation);
-  
     res.json(newReservation);
-  });
+  }
+  else {
+
+    waitingList.push(newReservation);
+    res.json(newReservation);
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
+  }
+});
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
